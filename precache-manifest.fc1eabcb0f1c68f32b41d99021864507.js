a3f1d15d3f86f5671c0fa9fec49000463f50ae07
DannyV99 <dgvonmoser@gmail.com>
2018-10-04 18:53:18 -0500

    Updates

---
+++ precache-manifest.fc1eabcb0f1c68f32b41d99021864507.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f75a57d64a8befc43af4",
+    "url": "/memoryGame/static/js/runtime~main.f75a57d6.js"
+  },
+  {
+    "revision": "a2f782134b346ef74604",
+    "url": "/memoryGame/static/js/main.a2f78213.chunk.js"
+  },
+  {
+    "revision": "06a6492ab3c5b29693c9",
+    "url": "/memoryGame/static/js/1.06a6492a.chunk.js"
+  },
+  {
+    "revision": "a2f782134b346ef74604",
+    "url": "/memoryGame/static/css/main.7e9608fb.chunk.css"
+  },
+  {
+    "revision": "9728263bf0711c4f5090261b6b81bf25",
+    "url": "/memoryGame/index.html"
+  }
+];
